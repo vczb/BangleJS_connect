864ec33e54d06cb1fa4f3138723e52bf61b3d23d
Vini <viniczb@gmail.com>
2024-05-25 15:34:01 +0200

    Create dbus connector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use dbus::blocking::Connection;
+use dbus::message::MatchRule;
+use std::error::Error;
+use std::time::Duration;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let conn = Connection::new_system()?;
+
+    let rule = MatchRule::new_signal("org.freedesktop.DBus.Properties", "PropertiesChanged");
+
+    conn.add_match(rule, move |_: (), _, msg| {
+        println!("Received signal: {:?}", msg);
+        let test = msg.get_items();
+        println!("test: {:?}", test);
+        true
+    })?;
+
+    loop {
+        conn.process(Duration::from_millis(1000))?;
+    }
 }

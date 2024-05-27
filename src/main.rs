3c418c66128fb37ea5542ef18c85c23275998087
Vini <viniczb@gmail.com>
2024-05-27 20:38:08 +0200

    Refactor methods to utils

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use dbus::arg::messageitem::MessageItem;
 use dbus::blocking::Connection;
 use dbus::message::MatchRule;
 use std::error::Error;
 use std::time::Duration;
+use utils::{bytes_to_string, dbus_to_bytes};
 
-fn unbox<T>(value: Box<T>) -> T {
-    *value
-}
+mod utils;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let conn = Connection::new_system()?;
@@ -16,32 +14,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     conn.add_match(rule, move |_: (), _, msg| {
         let items = msg.get_items();
 
-        if let Some(MessageItem::Str(source)) = items.first() {
-            if source.contains("GattCharacteristic") {
-                if let Some(MessageItem::Dict(dict_items)) = items.get(1) {
-                    let mut bytes = Vec::new();
-                    for (_key, value) in dict_items.iter() {
-                        if let MessageItem::Variant(variant) = value {
-                            if let MessageItem::Array(array) = unbox(variant.clone()) {
-                                for byte in array.iter() {
-                                    if let MessageItem::Byte(b) = byte {
-                                        bytes.push(*b);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    match String::from_utf8(bytes) {
-                        Ok(payload) => {
-                            println!("Deserialized payload: {:?}", payload);
-                        }
-                        Err(e) => {
-                            eprintln!("Failed to deserialize binary data: {:?}", e);
-                        }
-                    }
-                }
-            }
-        }
+        let bytes = dbus_to_bytes(items);
+        let string_data = bytes_to_string(bytes);
+        println!("{:?}", string_data);
 
         true
     })?;
---
+++ src/utils.rs
@@ -0,0 +1,37 @@
+use dbus::arg::messageitem::MessageItem;
+
+fn unbox<T>(value: Box<T>) -> T {
+    *value
+}
+
+pub fn dbus_to_bytes(items: Vec<MessageItem>) -> Vec<u8> {
+    let mut bytes = Vec::new();
+    if let Some(MessageItem::Str(source)) = items.first() {
+        if source.contains("GattCharacteristic") {
+            if let Some(MessageItem::Dict(dict_items)) = items.get(1) {
+                for (_key, value) in dict_items.iter() {
+                    if let MessageItem::Variant(variant) = value {
+                        if let MessageItem::Array(array) = unbox(variant.clone()) {
+                            for byte in array.iter() {
+                                if let MessageItem::Byte(b) = byte {
+                                    bytes.push(*b);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return bytes;
+}
+
+pub fn bytes_to_string(bytes: Vec<u8>) -> String {
+    match String::from_utf8(bytes) {
+        Ok(payload) => return payload,
+        Err(e) => {
+            eprintln!("Failed to deserialize binary data: {:?}", e);
+            "".to_string()
+        }
+    }
+}
